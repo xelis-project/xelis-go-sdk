3c6f74e49ad5b4542f241f7831bc23837f9db912
Slixe <slixeprivate@gmail.com>
2025-11-30 20:51:07 +0100

    getwork: add TopoHeight, MinerWork

--- getwork/struct.go
+++ getwork/struct.go
@@ -1,5 +1,7 @@
 package getwork
 
+import "github.com/xelis-project/xelis-go-sdk/daemon"
+
 const (
 	NewJob        string = `new_job`
 	BlockAccepted string = `block_accepted`
@@ -9,8 +11,17 @@ const (
 )
 
 type BlockTemplate struct {
-	Algorithm  string `json:"algorithm"`
-	Difficulty string `json:"difficulty"`
-	Height     uint64 `json:"height"`
-	Template   string `json:"template"`
+	Algorithm  daemon.AlgorithmVersion `json:"algorithm"`
+	Difficulty string                  `json:"difficulty"`
+	Height     uint64                  `json:"height"`
+	TopoHeight uint64                  `json:"topoheight"`
+	Template   string                  `json:"template"`
+}
+
+type MinerWork struct {
+	Algorithm  daemon.AlgorithmVersion `json:"algorithm"`
+	MinerWork  string                  `json:"miner_work"`
+	Height     uint64                  `json:"height"`
+	Difficulty string                  `json:"difficulty"`
+	TopoHeight uint64                  `json:"topoheight"`
 }
